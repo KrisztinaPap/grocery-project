90d71904786e95aece920cdaad7bf72c63518eaf
Krisztina Pap <krisztina.pap@outlook.com>
2020-10-19 12:46:12 -0600

    New build

---
+++ build/precache-manifest.ce4f03c56eb4fcf31a06a732cd2cf3e7.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3ae0cd08d0efa03ac16244e651b4447d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b33acecabe55ff2c6e8f",
+    "url": "/static/css/main.32b773aa.chunk.css"
+  },
+  {
+    "revision": "7418db5e18f68b1f8086",
+    "url": "/static/js/2.bf9b6878.chunk.js"
+  },
+  {
+    "revision": "42cdbb2a7e1665f0166d79146c6d10a2",
+    "url": "/static/js/2.bf9b6878.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b33acecabe55ff2c6e8f",
+    "url": "/static/js/main.612a5d5c.chunk.js"
+  },
+  {
+    "revision": "508f19642b20819fce72",
+    "url": "/static/js/runtime-main.e3011c62.js"
+  },
+  {
+    "revision": "a003b5f243c2be10562e5fc2275994c0",
+    "url": "/static/media/Krisztina.a003b5f2.png"
+  },
+  {
+    "revision": "2928265b9b3a545b0f52e20cd31910e3",
+    "url": "/static/media/Lindsey.2928265b.jpg"
+  },
+  {
+    "revision": "6310ab600ee367123f88df07cbb058c6",
+    "url": "/static/media/Tosin.6310ab60.JPG"
+  },
+  {
+    "revision": "33e882df8c8121684116f773f48c8c19",
+    "url": "/static/media/footer-bg.33e882df.jpg"
+  },
+  {
+    "revision": "ac4939bd193a7f72389af83c40512200",
+    "url": "/static/media/header-bg.ac4939bd.jpg"
+  },
+  {
+    "revision": "4415bc6c0b62be7504b8265981aac347",
+    "url": "/static/media/html5.4415bc6c.png"
+  },
+  {
+    "revision": "3504d03830aae77f5291c9e6dface95e",
+    "url": "/static/media/landing-page.3504d038.jpg"
+  },
+  {
+    "revision": "d3d08186063be8e7f3b4c80275496d8f",
+    "url": "/static/media/react-redux.d3d08186.png"
+  },
+  {
+    "revision": "70f87bba34487dc1c0cb8916d800c63b",
+    "url": "/static/media/react.70f87bba.png"
+  },
+  {
+    "revision": "59c467536111a981d904a050d335c2be",
+    "url": "/static/media/redux.59c46753.png"
+  },
+  {
+    "revision": "70318b91320fcc9f031feb7a7711f376",
+    "url": "/static/media/scenario-addsingredients.70318b91.PNG"
+  },
+  {
+    "revision": "c6cf50e49ff2ecba97249eb44459876b",
+    "url": "/static/media/scenario-addsowningredients.c6cf50e4.PNG"
+  },
+  {
+    "revision": "c6b225a9d02faa396112fd83cfde60f0",
+    "url": "/static/media/scenario-beefpie.c6b225a9.PNG"
+  },
+  {
+    "revision": "21fe3b4d36a9f87342079c4f61ea952a",
+    "url": "/static/media/scenario-printscreen.21fe3b4d.PNG"
+  },
+  {
+    "revision": "cf5755fcbf7f8af6632f7a1d79d01e6e",
+    "url": "/static/media/scenario-signin.cf5755fc.PNG"
+  }
+]);
